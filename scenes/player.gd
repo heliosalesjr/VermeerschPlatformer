22c8f3fe5addf0ebb3f743cccd9f166c42838bce
Helio Sales Jr <heliosalesjr@gmail.com>
2023-10-12 17:37:40 -0300

    added the fall animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta):
 	Move(delta)
 	velocity.y += gravity
 	move_and_slide()
-	Jump()
+
 		
 func Move(delta):
 	var movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -29,7 +29,17 @@ func Move(delta):
 	if movement == 0.0:
 		velocity.x = 0.0
 		$AnimationPlayer.play("Idle")
+		
+	if is_on_floor() && Input.is_action_just_pressed("ui_jump"):
+		Jump()
+	
+	if !is_on_floor():
+		$AnimationPlayer.play("Jump")
+		
+	if !is_on_floor() && velocity.y > 10:
+		$AnimationPlayer.play("Fall")
 
 func Jump():
-	if Input.is_action_just_pressed("ui_jump"):
+#	if Input.is_action_just_pressed("ui_jump"):
 		velocity.y -= jump
+		#$AnimationPlayer.play("Jump")
