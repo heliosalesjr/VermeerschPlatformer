c19f6787ed258cf2ccbf88f7295c54a2182ad23f
Helio Sales Jr <heliosalesjr@gmail.com>
2023-11-05 00:15:38 -0300

    added a health system for the crates

--- scenes/Crate.gd
+++ scenes/Crate.gd
@@ -1,5 +1,13 @@
 extends StaticBody2D
 
+var health = 3
 
 func _on_hitbox_area_entered(area):
-	print("migranas")
+	if area.name == "Sword":
+		$anim.play("hurt")
+		health -= 1
+		await $anim.animation_finished
+		$anim.play("active")
+		if health <= 0:
+			$anim.play("destroyed")
+			queue_free()
